ac472276cbfbc7dfc2b2d20cb06bf576a18a4978
alankelly <alankelly@live.com>
2014-01-14 18:35:02 +0100

    freq must be cast to a double otherwise strange results are returned on arm

--- modules/test/benchmark/include/nt2/sdk/bench/metric/gflops.hpp
+++ modules/test/benchmark/include/nt2/sdk/bench/metric/gflops.hpp
@@ -14,7 +14,7 @@
 #include <nt2/sdk/bench/metric/speedup.hpp>
 #include <nt2/sdk/timing/now.hpp>
 #include <string>
-
+#include <iostream>
 namespace nt2 { namespace bench
 {
   /*!
@@ -35,7 +35,7 @@ namespace nt2 { namespace bench
                       ) const
     {
       unsigned long freq = get_cpu_freq();
-      double m = ( e.flops() * (double)freq / (Stat::evaluate(c) * 1000000000. ));
+      double m = ( e.flops() * e.size() * (double)freq / (Stat::evaluate(c) * 1000000000. ));
       details::measures_map[details::identify_result(name,e,*this)] = m;
       return m;
     }
