a8464bee9a53e65ebc26dbeb54edc0e6bcac01a3
atrantan <antoine.trantan@lri.fr>
2014-01-20 01:36:12 +0100

    Use shared_ptr<void> to enable futures chaining

--- modules/arch/tbb/include/nt2/sdk/tbb/future/future.hpp
+++ modules/arch/tbb/include/nt2/sdk/tbb/future/future.hpp
@@ -22,6 +22,7 @@
 #include <boost/move/move.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/make_shared.hpp>
+#include <boost/utility/result_of.hpp>
 
 #include <boost/preprocessor/cat.hpp>
 #include <boost/preprocessor/repetition/enum.hpp>
