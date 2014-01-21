8d9b5a8ee8cc19b6d5c59b10e5d06cf9e217781a
Mathias Gaunard <mathias@gaunard.com>
2014-01-21 16:18:15 +0100

    missing BOOST_SIMD_EXT_NS for fusion_sequence_

--- modules/sdk/meta/include/nt2/sdk/meta/fusion.hpp
+++ modules/sdk/meta/include/nt2/sdk/meta/fusion.hpp
@@ -14,7 +14,7 @@
 #include <nt2/sdk/meta/primitive_of.hpp>
 #include <boost/dispatch/meta/fusion.hpp>
 
-namespace nt2 { namespace ext
+namespace nt2 { namespace BOOST_SIMD_EXT_NS
 {
   using boost::dispatch::meta::fusion_sequence_;
   using boost::dispatch::meta::array_;
