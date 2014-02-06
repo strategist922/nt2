14dcb0fc9900b5c740e5d9da570fb4b9d56f47a2
Antoine TRAN TAN <antoine.tran-tan@live.fr>
2014-02-06 20:24:28 +0100

    Define specific data with futures

---
+++ modules/arch/shared_memory/include/nt2/sdk/shared_memory/settings/container_has_futures.hpp
@@ -0,0 +1,48 @@
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2011   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_SDK_SHARED_MEMORY_SETTINGS_CONTAINER_HAS_FUTURES_HPP_INCLUDED
+#define NT2_SDK_SHARED_MEMORY_SETTINGS_CONTAINER_HAS_FUTURES_HPP_INCLUDED
+
+#include <nt2/sdk/shared_memory/future.hpp>
+#include <boost/swap.hpp>
+#include <vector>
+
+
+namespace nt2 { namespace details {
+
+  template <typename Arch>
+  struct container_has_futures
+  {
+    typedef typename nt2::make_future<Arch,int >::type future;
+
+    inline void swap(container_has_futures& src)
+    {
+      boost::swap(futures_,src.futures_);
+      boost::swap(grain_,src.grain_);
+    }
+
+    inline void synchronize()
+    {
+        for(std::size_t n=0;n<futures_.size();++n)
+        {
+            futures_[n].get();
+        }
+    }
+
+    //===========================================
+    // vector of Futures
+    //===========================================
+    std::vector<future> futures_;
+    std::size_t grain_;
+
+  };
+
+} }
+
+#endif
