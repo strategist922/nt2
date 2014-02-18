6c13d52f2773239fd1acc2bdae5a4382fb4a568a
Antoine TRAN TAN <antoine.tran-tan@live.fr>
2014-02-18 13:25:19 +0100

    Manage synchronization of dependences and dependents

--- modules/arch/shared_memory/include/nt2/sdk/shared_memory/details/proto_data_with_futures.hpp
+++ modules/arch/shared_memory/include/nt2/sdk/shared_memory/details/proto_data_with_futures.hpp
@@ -14,18 +14,24 @@
 
 namespace nt2 { namespace details {
 
-    template<class Future>
+    template<class Future,class Specifics>
     struct proto_data_with_futures
     {
         typedef typename std::vector<Future> FutureVector;
 
-        proto_data_with_futures(std::size_t begin,std::size_t size)
-        :begin_(begin),size_(size)
+        proto_data_with_futures(std::size_t begin
+                               ,std::size_t size
+                               ,Specifics & specifics)
+        :begin_(begin),size_(size),specifics_(specifics)
         {}
 
         FutureVector futures_;
         std::size_t begin_;
         std::size_t size_;
+        Specifics & specifics_;
+
+        private:
+        proto_data_with_futures& operator=(proto_data_with_futures const&);
 
     };
 
