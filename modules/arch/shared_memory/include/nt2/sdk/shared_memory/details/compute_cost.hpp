9ef5f2f2457adb5ea355b5fb402ff1f3b7a68bb5
Antoine TRAN TAN <antoine.tran-tan@live.fr>
2014-09-30 19:09:17 +0200

    Change the computation of cost

--- modules/arch/shared_memory/include/nt2/sdk/shared_memory/details/compute_cost.hpp
+++ modules/arch/shared_memory/include/nt2/sdk/shared_memory/details/compute_cost.hpp
@@ -10,7 +10,10 @@
 #define NT2_SDK_SHARED_MEMORY_DETAILS_COMPUTE_COST_HPP_INCLUDED
 
 #include <nt2/sdk/meta/is_container.hpp>
+#include <nt2/sdk/config/cache.hpp>
 #include <nt2/sdk/shared_memory/details/aggregate_costs.hpp>
+#include <nt2/sdk/shared_memory/thread_utility.hpp>
+#include <nt2/sdk/shared_memory/runtime_costs.hpp>
 
 #include <algorithm>
 #include <set>
@@ -21,16 +24,33 @@
 
 namespace nt2 { namespace details
 {
-    template < class In, class Out >
-    inline bool compute_cost(In & in, Out & out)
+    template < class Tag, class Arch, class Out, class In >
+    inline bool compute_cost(Out & out, In & in)
     {
       typedef typename In::const_pointer raw_type;
+      typedef typename nt2::runtime_costs<Tag,Arch>::type skel_cost_type;
+
+      std::size_t nin  = nt2::numel(in);
+      std::size_t nout = nt2::numel(out);
+
+      std::size_t cache_size
+        = config::top_cache_size(3)/sizeof(typename Out::value_type);
 
       std::set< raw_type > terminal_set;
       details::aggregate_terminals()(in,  0, terminal_set);
-      details::aggregate_terminals()(out, 0, terminal_set);
 
-      return terminal_set.size() < details::aggregate_costs()(in);
+      std::size_t access_cost  = terminal_set.size() * nin + nout;
+      std::size_t compute_cost = details::aggregate_costs()(in) * nin;
+      std::size_t cache_cost = (access_cost/cache_size)*100;
+      std::size_t skel_cost  = skel_cost_type();
+
+      std::size_t sequential = access_cost + compute_cost + cache_cost;
+
+      std::size_t parallel
+        = (access_cost + compute_cost) / nt2::get_num_threads()
+        + cache_cost + skel_cost;
+
+      return parallel < sequential;
     }
 
 } }
