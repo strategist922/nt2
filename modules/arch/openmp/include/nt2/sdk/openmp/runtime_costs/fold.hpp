985fe96f6be6990dc7bd4b72df7f341a2e567402
Antoine TRAN TAN <antoine.tran-tan@live.fr>
2014-09-30 18:59:41 +0200

    Typedef of spawner costs for openmp

---
+++ modules/arch/openmp/include/nt2/sdk/openmp/runtime_costs/fold.hpp
@@ -0,0 +1,38 @@
+//==============================================================================
+//         Copyright 2003 - 2011   LASMEA UMR 6602 CNRS/Univ. Clermont II
+//         Copyright 2009 - 2013   LRI    UMR 8623 CNRS/Univ Paris Sud XI
+//         Copyright 2012 - 2013   MetaScale SAS
+//
+//          Distributed under the Boost Software License, Version 1.0.
+//                 See accompanying file LICENSE.txt or copy at
+//                     http://www.boost.org/LICENSE_1_0.txt
+//==============================================================================
+#ifndef NT2_SDK_OPENMP_RUNTIME_COSTS_FOLD_HPP_INCLUDED
+#define NT2_SDK_OPENMP_RUNTIME_COSTS_FOLD_HPP_INCLUDED
+
+#if defined(_OPENMP) && _OPENMP >= 200203 /* OpenMP 2.0 */
+
+#include <nt2/sdk/runtime_costs.hpp>
+#include <nt2/sdk/shared_memory/runtime_costs.hpp>
+
+#ifndef BOOST_NO_EXCEPTIONS
+#include <boost/exception_ptr.hpp>
+#endif
+
+namespace nt2
+{
+  namespace tag
+    {
+      struct fold_;
+      template<class T> struct openmp_;
+    }
+
+  template<class Site>
+  struct runtime_costs< tag::fold_, tag::openmp_<Site> >
+  {
+    typedef openmp_fold type;
+  };
+}
+
+#endif
+#endif
