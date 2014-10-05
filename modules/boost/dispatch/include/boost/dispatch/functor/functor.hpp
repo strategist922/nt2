f5dbac65b9f9b9790ca583355a6997cb4e44849a
Joel Falcou <joel.falcou@lri.fr>
2014-10-05 17:56:20 +0200

    Clean-up doc of functor

--- modules/boost/dispatch/include/boost/dispatch/functor/functor.hpp
+++ modules/boost/dispatch/include/boost/dispatch/functor/functor.hpp
@@ -10,18 +10,6 @@
 #ifndef BOOST_DISPATCH_FUNCTOR_FUNCTOR_HPP_INCLUDED
 #define BOOST_DISPATCH_FUNCTOR_FUNCTOR_HPP_INCLUDED
 
-/*!
- * \file
- * \brief Implements Boost.Dispatch main functor handler
- */
-
-/*!
- * \defgroup functors Functor system
- * \ingroup sdk
- * This module gathers macros, classes and functions to define, implement
- * and specialize polymorphic functors.
- */
-
 #include <boost/config.hpp>
 #include <boost/type_traits/common_type.hpp>
 #include <boost/dispatch/meta/as_floating.hpp>
@@ -54,27 +42,26 @@
 
 namespace boost { namespace dispatch
 {
-  //============================================================================
   /*!
-   * \ingroup functors
-   * \ref functor is a generic, concept-dispatched polymorphic function object.
-   * Every function in Boost.Dispatch are defined in term of implementing a call strategy
-   * for \ref functor. The main rationale is to centralize and homogenize the way
-   * functions with multiple possible implementation with respect to types,
-   * architectures and compilation phases are handled.
-   *
-   * \tparam Tag Function tag to dispatch to.
-   * \tparam EvalContext Tag defining how to perform said function call with
-   * respect to architecture or phase settings.
-   *
-   * \par Models:
-   * \dco
-   *
-   * \see call
-   * \see hierarchy
-   * \see make_functor
-   */
-  //============================================================================
+    @brief Generic function object
+
+    functor is a generic, concept-dispatched polymorphic function object.
+    Every function in Boost.Dispatch are defined in term of implementing a
+    call strategy for functor. The main rationale is to centralize and homogenize
+    the way functions with multiple possible implementation with respect to types,
+    architectures and compilation phases are handled.
+
+    @tparam Tag Function tag to dispatch to.
+    @tparam EvalContext Tag defining how to perform said function call with
+    respect to architecture or phase settings.
+
+    @par Models:
+    @dco
+
+    @see call
+    @see hierarchy
+    @see make_functor
+  **/
   template<class Tag, class EvalContext> struct functor
   {
     typedef Tag         tag_type;
@@ -99,16 +86,13 @@ namespace boost { namespace dispatch
 #endif
     {};
 
-    //==========================================================================
     /*!
-     * Polymorphic variadic function call operator. This catch-all operators
-     * dispatches its arguments to the proper type and architecture based
-     * implementation using the hierarchy_of system.
-     *
-     * \param args Arguments of function \c Tag implementation
-     * \return The result of the calculation of function \c Tag
-     */
-    //==========================================================================
+      Polymorphic function call operator using hierarchies to select its own
+      implementation.
+
+      @param  args Functor call arguments
+      @return The result of the application of current function on args.
+    **/
     template<class... Args> BOOST_FORCEINLINE
     typename result<functor(typename meta::as_ref<Args>::type...)>::type
     operator()( Args&& ...args ) const
