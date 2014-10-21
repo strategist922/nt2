9b2615a277cdd27fd9c025c379596de5c36df65b
Antoine TRAN TAN <antoine.tran-tan@live.fr>
2014-10-21 16:38:31 +0200

    Simplify microbench script

--- modules/arch/shared_memory/script/run_microbenchmarks.sh
+++ modules/arch/shared_memory/script/run_microbenchmarks.sh
@@ -3,43 +3,35 @@
 tmp="${1}/tmp"
 out="${1}/include/nt2/sdk/runtime_costs.hpp"
 
-# Normal .hpp generation
-if [ -f $out ]; then
-  echo -e "#ifndef NT2_SDK_RUNTIME_COSTS_HPP_INCLUDED" > $tmp
-  echo -e "#define NT2_SDK_RUNTIME_COSTS_HPP_INCLUDED\n" >> $tmp
-  opt="-t `grep --count processor /proc/cpuinfo`"
-  for nt2_arch in "openmp" "hpx" "tbb"
-  do
-    for skel in "transform" "fold" "scan"
-    do
-      next_exe="bench/arch.${nt2_arch}.microbench.${skel}.bench"
-      echo -n "typedef typename boost::mpl::size_t< " >> ${tmp}
-      if [ -f ${next_exe} ]; then
-        echo -n "(std::size_t) " >> ${tmp}
-        echo -n `./${next_exe} ${opt} | grep "cpe" | cut -f 3` >> ${tmp}
-      else
-        echo -n "0" >> ${tmp}
-      fi
-      echo -e " > ${nt2_arch}_${skel};" >> ${tmp}
-    done
-    echo "" >> ${tmp}
-  done
-  echo -e "\n#endif" >> $tmp
-  mv $tmp $out
-
 # First .hpp generation
+if [ -d "${1}/include_tmp" ]; then
+  tmp="${1}/include_tmp/nt2/sdk/runtime_costs.hpp"
+# Normal .hpp generation
 else
-  out="${1}/include_tmp/nt2/sdk/runtime_costs.hpp"
-  echo -e "#ifndef NT2_SDK_RUNTIME_COSTS_HPP_INCLUDED" > $out
-  echo -e "#define NT2_SDK_RUNTIME_COSTS_HPP_INCLUDED\n" >> $out
-  echo -e "#include <boost/mpl/size_t.hpp>\n" >> $out
-  for nt2_arch in "openmp" "hpx" "tbb"
+  tmp="${1}/tmp"
+fi
+
+echo -e "#ifndef NT2_SDK_RUNTIME_COSTS_HPP_INCLUDED" > $tmp
+echo -e "#define NT2_SDK_RUNTIME_COSTS_HPP_INCLUDED\n" >> $tmp
+opt="-t `grep --count processor /proc/cpuinfo`"
+for nt2_arch in "openmp" "hpx" "tbb"
+do
+  for skel in "transform" "fold" "scan"
   do
-    for skel in "transform" "fold" "scan"
-    do
-      echo "typedef typename boost::mpl::size_t< 0 > ${nt2_arch}_${skel};" >> ${out}
-    done
-    echo "" >> ${out}
+    next_exe="bench/arch.${nt2_arch}.microbench.${skel}.bench"
+    echo -n "typedef typename boost::mpl::size_t< " >> ${tmp}
+    if [ -f ${next_exe} ]; then
+      echo -n "(std::size_t) " >> ${tmp}
+      echo -n `./${next_exe} ${opt} | grep "cpe" | cut -f 3` >> ${tmp}
+    else
+      echo -n "0" >> ${tmp}
+    fi
+    echo -e " > ${nt2_arch}_${skel};" >> ${tmp}
   done
-  echo -e "\n#endif" >> $out
+  echo "" >> ${tmp}
+done
+echo -e "\n#endif" >> $tmp
+
+if [ ! -d "${1}/include_tmp" ]; then
+  mv $tmp $out
 fi
