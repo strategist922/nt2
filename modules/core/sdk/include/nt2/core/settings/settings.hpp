21fe3c3b15a8e267c3b8c464453db3cf46cba383
Joel Falcou <joel.falcou@lri.fr>
2014-11-18 18:36:49 +0100

    Cleanup of settings.hpp
    - settings(...) is now variadic
    - Removed forward file for settings

--- modules/core/sdk/include/nt2/core/settings/settings.hpp
+++ modules/core/sdk/include/nt2/core/settings/settings.hpp
@@ -11,66 +11,58 @@
 
 #include <nt2/core/settings/option.hpp>
 #include <nt2/core/settings/match_option.hpp>
-#include <nt2/core/settings/forward/settings.hpp>
 
 #include <boost/mpl/deref.hpp>
 #include <boost/mpl/vector.hpp>
 #include <boost/mpl/find_if.hpp>
 #include <boost/dispatch/meta/any.hpp>
-#include <boost/preprocessor/repetition/enum_params.hpp>
-#include <boost/preprocessor/repetition/repeat_from_to.hpp>
 
-//==============================================================================
-/*!
- * Defines the maximum number of options that can be passed to a composite
- * settings. This macro can be set by users if needed.
- **/
-//==============================================================================
-#ifndef NT2_META_MAX_OPTIONS_NB
-#define NT2_META_MAX_OPTIONS_NB 12
-#endif
-
-namespace nt2 { namespace meta
+namespace nt2
 {
-  //============================================================================
-  // settings list of one argument return the argument option value
-  //============================================================================
-  template<class S0, class Option, class Semantic>
-  struct  option< settings(S0), Option, Semantic >
-        : option<S0,Option,Semantic>
-  {};
+  /*!
+    @brief Settings pack mark-up type
 
-  template< class S0, class Option>
-  struct  match_option< settings(S0), Option >
-        : match_option<S0,Option>
-  {};
+    settings is a seed type that allow construction of multiple options pack.
+    If given container needs to have options A and B set, settings(A,B) will
+    perform such a task.
+  **/
+  struct settings {};
 
-  //============================================================================
-  // Non-empty settings lists are parse using boost::mpl::find_if
-  //============================================================================
-  #define M0(z,n,t)                                                            \
-  template< BOOST_PP_ENUM_PARAMS(n,class S), class Option>                     \
-  struct  match_option< settings(BOOST_PP_ENUM_PARAMS(n,S)), Option >          \
-        : boost::dispatch::meta::any< lambda_match_option<Option>              \
-                                    , BOOST_PP_ENUM_PARAMS(n,S)                \
-                                    >                                          \
-  {};                                                                          \
-                                                                               \
-  template< BOOST_PP_ENUM_PARAMS(n,class S), class Option, class Semantic>     \
-  struct option < settings(BOOST_PP_ENUM_PARAMS(n,S)), Option, Semantic >      \
-  {                                                                            \
-    typedef boost::mpl::vector##n<BOOST_PP_ENUM_PARAMS(n,S)>  options_t;       \
-    typedef typename boost::mpl::find_if< options_t                            \
-                                        , lambda_match_option<Option>          \
-                                        >::type               it_t;            \
-    typedef typename boost::mpl::deref<it_t>::type            found_t;         \
-    typedef typename option<found_t,Option,Semantic>::type    type;            \
-  };                                                                           \
-  /**/
+  namespace meta
+  {
+    /// INTERNAL ONLY
+    template< class S0, class Option>
+    struct  match_option< settings(S0), Option >
+          : match_option<S0,Option>
+    {};
 
-  BOOST_PP_REPEAT_FROM_TO(2,NT2_META_MAX_OPTIONS_NB,M0,~)
+    /// INTERNAL ONLY
+    template<typename Option, typename S0, typename S1, typename... S>
+    struct  match_option< settings(S0,S1,S...), Option >
+          : boost::dispatch::meta::any< lambda_match_option<Option>
+                                      , S0, S1, S...
+                                      >
+    {};
 
-  #undef M0
-} }
+    /// INTERNAL ONLY
+    template<class S0, class Option, class Semantic>
+    struct  option< settings(S0), Option, Semantic >
+          : option<S0,Option,Semantic>
+    {};
+
+    /// INTERNAL ONLY
+    template< typename Option, typename Semantic
+            , typename S0, typename S1, typename... S
+            >
+    struct option<settings(S0,S1,S...), Option, Semantic>
+    {
+      typedef typename boost::mpl::find_if< boost::mpl::vector<S0,S1,S...>
+                                          , lambda_match_option<Option>
+                                          >::type               it_t;
+      typedef typename boost::mpl::deref<it_t>::type            found_t;
+      typedef typename option<found_t,Option,Semantic>::type    type;
+    };
+  }
+}
 
 #endif
