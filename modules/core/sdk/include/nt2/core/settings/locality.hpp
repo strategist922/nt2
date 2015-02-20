18d3eddcff6c622084bef8d51e9753b124ef474f
masliah <ian.masliah@u-psud.fr>
2015-02-20 17:14:46 +0100

    added locality based metafunctions

--- modules/core/sdk/include/nt2/core/settings/locality.hpp
+++ modules/core/sdk/include/nt2/core/settings/locality.hpp
@@ -8,7 +8,10 @@
 #ifndef NT2_CORE_SETTINGS_LOCALITY_HPP_INCLUDED
 #define NT2_CORE_SETTINGS_LOCALITY_HPP_INCLUDED
 
+#include <nt2/core/settings/option.hpp>
 #include <nt2/core/settings/forward/locality.hpp>
+#include <boost/mpl/or.hpp>
+#include <type_traits>
 
 namespace nt2 { namespace tag
 {
@@ -30,6 +33,35 @@ namespace nt2 { namespace tag
   struct locality_::apply<nt2::device_>
                         : boost::mpl::true_
   {};
+} }
+
+namespace nt2 { namespace meta
+{
+
+  template<typename A,typename B>
+  struct is_same_locality : std::is_same<typename option<A,tag::locality_>::type
+                                        ,typename option<B,tag::locality_>::type
+                                        >
+  {};
+
+  template<typename X>
+  struct is_on_device : std::is_same< typename option<X,tag::locality_>::type
+                                    , nt2::device_
+                                    >
+  {};
+
+  template<typename X>
+  struct is_on_host : std::is_same< typename option<X,tag::locality_>::type
+                                  , nt2::host_
+                                  >
+  {};
+
+  template<typename A0, typename A1>
+  struct is_device_assign : boost::mpl::or_< is_on_device<A0>
+                                           , is_on_device<A1>
+                                           >
+  {};
+
 } }
 
 #include <nt2/core/settings/details/locality.hpp>
