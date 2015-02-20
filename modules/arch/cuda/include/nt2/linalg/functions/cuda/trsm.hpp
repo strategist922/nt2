465ddbac909641ab1070f1e5f0b5716dd89afa2c
masliah <ian.masliah@u-psud.fr>
2015-02-20 17:37:15 +0100

    corrections on cuda trsm

--- modules/arch/cuda/include/nt2/linalg/functions/cuda/trsm.hpp
+++ modules/arch/cuda/include/nt2/linalg/functions/cuda/trsm.hpp
@@ -16,6 +16,7 @@
 
 #include <nt2/dsl/functions/terminal.hpp>
 #include <nt2/core/container/table/kind.hpp>
+#include <nt2/sdk/meta/device.hpp>
 
 #include <nt2/linalg/details/utility/f77_wrapper.hpp>
 #include <nt2/linalg/details/blas/blas3.hpp>
@@ -24,10 +25,10 @@
 #include <nt2/include/functions/width.hpp>
 #include <complex>
 #include <cublas.h>
-#include <iostream>
 
 namespace nt2 { namespace ext
 {
+
   BOOST_DISPATCH_IMPLEMENT ( trsm_, nt2::tag::cuda_<site>
                             , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(site)
                             , (scalar_< ints8_<A0> >)
@@ -46,92 +47,15 @@ namespace nt2 { namespace ext
         int  m  = nt2::height(a);
         int  n  = nt2::width(b);
         int  lda = (side=='L'||'l') ? m : n ;
-
         double alpha = 1.;
+
+        auto device_a = to_device(a);
+        auto device_b = to_device(b);
 
         cublasDtrsm( side, uplo, transa, diag, m, n, alpha
-                          , a.data(),lda,b.data(),m);
+                   , device_a.data(), lda, device_b.data(), m);
 
-     }
-  };
-
-  BOOST_DISPATCH_IMPLEMENT( trsm_,  nt2::tag::cuda_<site>
-                            , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(site)
-                            , (scalar_< ints8_<A0> >)
-                              (scalar_< ints8_<A1> >)
-                              (scalar_< ints8_<A2> >)
-                              (scalar_< ints8_<A3> >)
-                              ((container_< nt2::tag::table_, single_<A4>, S4 >))
-                              ((container_< nt2::tag::table_, single_<A5>, S5 >))
-                            )
-  {
-    typedef void  result_type;
-
-     BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
-                                             , A3 const& diag, A4 const& a, A5& b ) const
-     {
-        int  m  = nt2::height(a);
-        int  n  = nt2::width(b);
-        int  lda = (side=='L'||'l') ? m : n ;
-
-        double alpha = 1.;
-
-        cublasStrsm( side, uplo, transa, diag, m, n, alpha
-                          , a.data(),lda,b.data(),m);
-
-     }
-  };
-
-  BOOST_DISPATCH_IMPLEMENT( trsm_,  nt2::tag::cuda_<site>
-                            , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(site)
-                            , (scalar_< ints8_<A0> >)
-                              (scalar_< ints8_<A1> >)
-                              (scalar_< ints8_<A2> >)
-                              (scalar_< ints8_<A3> >)
-                              ((container_< nt2::tag::table_, complex_<double_<A4> >, S4 >))
-                              ((container_< nt2::tag::table_, complex_<double_<A5> >, S5 >))
-                            )
-  {
-    typedef void  result_type;
-
-     BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
-                                             , A3 const& diag, A4 const& a, A5& b ) const
-     {
-        int  m  = nt2::height(a);
-        int  n  = nt2::width(b);
-        int  lda = (side=='L'||'l') ? m : n ;
-
-        double alpha = 1.;
-
-        cublasZtrsm( side, uplo, transa, diag, m, n, alpha
-                          , a.data(),lda,b.data(),m);
-
-     }
-  };
-
-  BOOST_DISPATCH_IMPLEMENT( trsm_,  nt2::tag::cuda_<site>
-                            , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(site)
-                            , (scalar_< ints8_<A0> >)
-                              (scalar_< ints8_<A1> >)
-                              (scalar_< ints8_<A2> >)
-                              (scalar_< ints8_<A3> >)
-                              ((container_< nt2::tag::table_, complex_<single_<A4> >, S4 >))
-                              ((container_< nt2::tag::table_, complex_<single_<A5> >, S5 >))
-                            )
-  {
-    typedef void  result_type;
-
-     BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
-                                             , A3 const& diag, A4 const& a, A5& b ) const
-     {
-        nt2_la_int  m  = nt2::height(a);
-        nt2_la_int  n  = nt2::width(b);
-        nt2_la_int  lda = (side=='L'||'l')? m : n ;
-        std::complex<float> alpha(1.0,0.);
-
-        NT2_F77NAME(ctrsm)( &side, &uplo, &transa, &diag, &m, &n, &alpha
-                          , a.raw(),&lda,b.raw(),&m);
-
+        device_swap(device_b,b);
      }
   };
 
@@ -156,9 +80,108 @@ namespace nt2 { namespace ext
         int  n  = nt2::width(b);
         int  lda = (side=='L'||'l') ? m : n ;
 
-        cublasDtrsm( side, uplo, transa, diag, m, n, alpha
-                          , a.data(),lda,b.data(),m);
+        auto device_a = to_device(a);
+        auto device_b = to_device(b);
 
+        cublasDtrsm( side, uplo, transa, diag, m, n, alpha
+                   , device_a.data(), lda, (double*)device_b.data(), m);
+
+        device_swap(device_b,b);
+     }
+  };
+
+  BOOST_DISPATCH_IMPLEMENT( trsm_,  nt2::tag::cuda_<site>
+                            , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(site)
+                            , (scalar_< ints8_<A0> >)
+                              (scalar_< ints8_<A1> >)
+                              (scalar_< ints8_<A2> >)
+                              (scalar_< ints8_<A3> >)
+                              ((container_< nt2::tag::table_, single_<A4>, S4 >))
+                              ((container_< nt2::tag::table_, single_<A5>, S5 >))
+                            )
+  {
+    typedef void  result_type;
+
+     BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
+                                             , A3 const& diag, A4 const& a, A5& b ) const
+     {
+        int  m  = nt2::height(a);
+        int  n  = nt2::width(b);
+        int  lda = (side=='L'||'l') ? m : n ;
+
+        double alpha = 1.;
+
+        auto device_a = to_device(a);
+        auto device_b = to_device(b);
+
+        cublasStrsm( side, uplo, transa, diag, m, n, alpha
+                   , device_a.data(),lda,device_b.data(),m);
+
+        device_swap(device_b,b);
+     }
+  };
+
+  BOOST_DISPATCH_IMPLEMENT( trsm_,  nt2::tag::cuda_<site>
+                            , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(site)
+                            , (scalar_< ints8_<A0> >)
+                              (scalar_< ints8_<A1> >)
+                              (scalar_< ints8_<A2> >)
+                              (scalar_< ints8_<A3> >)
+                              ((container_< nt2::tag::table_, complex_<double_<A4> >, S4 >))
+                              ((container_< nt2::tag::table_, complex_<double_<A5> >, S5 >))
+                            )
+  {
+    typedef void  result_type;
+
+     BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
+                                             , A3 const& diag, A4 const& a, A5& b ) const
+     {
+        int  m  = nt2::height(a);
+        int  n  = nt2::width(b);
+        int  lda = (side=='L'||'l') ? m : n ;
+
+        cuDoubleComplex alpha {1. , 0};
+
+        auto device_a = to_device(a);
+        auto device_b = to_device(b);
+
+        cublasZtrsm( side, uplo, transa, diag, m, n, alpha
+                          , (cuDoubleComplex*)device_a.data(),lda
+                          , (cuDoubleComplex*)device_b.data(),m);
+
+        device_swap(device_b,b);
+     }
+  };
+
+  BOOST_DISPATCH_IMPLEMENT( trsm_,  nt2::tag::cuda_<site>
+                            , (A0)(A1)(A2)(A3)(A4)(S4)(A5)(S5)(site)
+                            , (scalar_< ints8_<A0> >)
+                              (scalar_< ints8_<A1> >)
+                              (scalar_< ints8_<A2> >)
+                              (scalar_< ints8_<A3> >)
+                              ((container_< nt2::tag::table_, complex_<single_<A4> >, S4 >))
+                              ((container_< nt2::tag::table_, complex_<single_<A5> >, S5 >))
+                            )
+  {
+    typedef void  result_type;
+
+     BOOST_FORCEINLINE result_type operator()( A0 const& side, A1 const& uplo, A2 const& transa
+                                             , A3 const& diag, A4 const& a, A5& b ) const
+     {
+        nt2_la_int  m  = nt2::height(a);
+        nt2_la_int  n  = nt2::width(b);
+        nt2_la_int  lda = (side=='L'||'l')? m : n ;
+
+        auto device_a = to_device(a);
+        auto device_b = to_device(b);
+
+        cuFloatComplex alphac {1.0,0};
+
+        cublasCtrsm( side, uplo, transa, diag, m, n, alphac
+                          , (cuFloatComplex*)device_a.data(),lda
+                          , (cuFloatComplex*)device_b.data(), m);
+
+        device_swap(device_b,b);
      }
   };
 
@@ -182,9 +205,13 @@ namespace nt2 { namespace ext
         int  n  = nt2::width(b);
         int  lda = (side=='L'||'l') ? m : n ;
 
-        cublasStrsm( side, uplo, transa, diag, m, n, alpha
-                          , a.data(),lda,b.data(),m);
+        auto device_a = to_device(a);
+        auto device_b = to_device(b);
 
+        cublasStrsm( side, uplo, transa, diag, m, n, alpha
+                          , device_a.data(),lda,(float*)device_b.data(),m);
+
+        device_swap(device_b,b);
      }
   };
 
@@ -209,9 +236,16 @@ namespace nt2 { namespace ext
         int  n  = nt2::width(b);
         int  lda = (side=='L'||'l') ? m : n ;
 
-        cublasZtrsm( side, uplo, transa, diag, m, n, alpha
-                          , a.data(),lda,b.data(),m);
+        auto device_a = to_device(a);
+        auto device_b = to_device(b);
 
+        cuDoubleComplex alphac {alpha.real(), alpha.imag()};
+
+        cublasZtrsm( side, uplo, transa, diag, m, n, alphac
+                          , (cuDoubleComplex*)device_a.data(),lda
+                          , (cuDoubleComplex*)device_b.data(),m);
+
+        device_swap(device_b,b);
 
      }
   };
@@ -237,8 +271,16 @@ namespace nt2 { namespace ext
         int  n  = nt2::width(b);
         int  lda = (side=='L'||'l') ? m : n ;
 
-        cublasCtrsm( side, uplo, transa, diag, m, n, alpha
-                          , a.data(),lda,b.data(),m);
+        auto device_a = to_device(a);
+        auto device_b = to_device(b);
+
+        cuFloatComplex alphac {alpha.real(), alpha.imag()};
+
+        cublasCtrsm( side, uplo, transa, diag, m, n, alphac
+                          , (cuFloatComplex*)device_a.data(),lda
+                          , (cuFloatComplex*)device_b.data(),m);
+
+        device_swap(device_b,b);
      }
   };
 
