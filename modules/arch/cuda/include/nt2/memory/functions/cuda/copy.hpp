268ea5ce3cc57fa44182d1e7e4df21dc9b4549ab
masliah <ian.masliah@u-psud.fr>
2015-02-20 17:34:05 +0100

    refactored copy and added an assign_swap for cuda

--- modules/arch/cuda/include/nt2/memory/functions/cuda/copy.hpp
+++ modules/arch/cuda/include/nt2/memory/functions/cuda/copy.hpp
@@ -8,56 +8,90 @@
 #ifndef NT2_MEMORY_FUNCTIONS_CUDA_COPY_HPP_INCLUDED
 #define NT2_MEMORY_FUNCTIONS_CUDA_COPY_HPP_INCLUDED
 
+#include <boost/dispatch/details/auto_decltype.hpp>
+
 #if defined(NT2_HAS_CUDA)
 
-#include <nt2/sdk/cuda/cuda.hpp>
 #include <nt2/sdk/memory/cuda/buffer.hpp>
+#include <nt2/core/settings/locality.hpp>
 #include <cublas.h>
 
 namespace nt2 { namespace memory
-  {
+{
+  template<typename In, typename Out>
+  struct copy_;
 
-  template<class T>
-  class cuda_buffer;
+  template<> struct copy_<device_,host_>
+  {
+    static BOOST_FORCEINLINE
+    BOOST_AUTO_DECLTYPE mode()
+    BOOST_AUTO_DECLTYPE_BODY ( cudaMemcpyDeviceToHost )
+  };
+
+  template<> struct copy_<host_,device_>
+  {
+    static BOOST_FORCEINLINE
+    BOOST_AUTO_DECLTYPE mode()
+    BOOST_AUTO_DECLTYPE_BODY ( cudaMemcpyHostToDevice )
+  };
+
+  template<> struct copy_<device_,device_>
+  {
+    static BOOST_FORCEINLINE
+    BOOST_AUTO_DECLTYPE mode()
+    BOOST_AUTO_DECLTYPE_BODY ( cudaMemcpyDeviceToDevice )
+  };
+
+  template<typename In, typename Out, typename HDI, typename HDO>
+  inline void copy( In const& a, Out& b , HDI const& , HDO const&
+                  , cudaStream_t stream = 0)
+  {
+    using T = typename Out::value_type;
+//TODO
+    CUDA_ERROR(cudaMemcpyAsync( (T*)b.data()
+     , a.data()
+     , a.size()* sizeof(T)
+     , copy_<HDI,HDO>::mode()
+     , stream
+     ));
+  }
 
   template<typename T, class C1>
   inline void copy(cuda_buffer<T> const& a, C1 &b, cudaStream_t stream = 0)
   {
     b.resize(a.size());
-
-    CUDA_ERROR(cudaMemcpyAsync( b.data()
-     , a.data()
-     , a.size()* sizeof(T)
-     , cudaMemcpyDeviceToHost
-     , stream
-     ));
+    copy(a,b,nt2::device_{},nt2::host_{},stream);
   }
 
   template< class C1,typename T>
   inline void copy(C1 const& a, cuda_buffer<T> &b, cudaStream_t stream = 0)
   {
     b.resize(a.size());
-
-    CUDA_ERROR(cudaMemcpyAsync( b.data()
-     , a.data()
-     , a.size()* sizeof(T)
-     , cudaMemcpyHostToDevice
-     , stream
-     ));
+    copy(a,b,nt2::host_{},nt2::device_{},stream);
   }
 
   template<typename T>
-  inline void copy(cuda_buffer<T> const& a, cuda_buffer<T> & b, cudaStream_t stream = 0 )
+  inline void copy( cuda_buffer<T> const& a, cuda_buffer<T> & b
+                  , cudaStream_t stream = 0 )
   {
     b.resize(a.size());
-
-    CUDA_ERROR(cudaMemcpyAsync( b.data()
-     , a.data()
-     , a.size()* sizeof(T)
-     , cudaMemcpyDeviceToDevice
-     , stream
-     ));
+    copy(a,b,nt2::device_{},nt2::device_{},stream);
   }
+
+
+}}
+
+namespace nt2 { namespace container
+{
+
+  template<class A0, class A1>
+  typename std::enable_if< meta::is_on_device<A0>::value
+                        || meta::is_on_device<A1>::value>::type
+  assign_swap(A0& a0, A1& a1)
+  {
+    device_swap(a0,a1);
+  }
+
 }}
 
 
