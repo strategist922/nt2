e10581c147e3e188b81644bda02893cbc7fc07f1
Charly Chevalier <charly.chevalier@numscale.com>
2015-06-16 11:39:57 +0200

    SSE1/SSE2: scalar: Split memory functions

--- modules/boost/simd/sdk/include/boost/simd/memory/functions/scalar/sse/stream.hpp
+++ modules/boost/simd/sdk/include/boost/simd/memory/functions/scalar/sse/stream.hpp
@@ -17,38 +17,7 @@
 
 namespace boost { namespace simd { namespace ext
 {
-  /// INTERNAL ONLY - Integer stream with offset
-  BOOST_DISPATCH_IMPLEMENT          ( stream_, tag::cpu_
-                                    , (A0)(A1)(A2)
-                                    , (scalar_< ints32_<A0> >)
-                                      (iterator_< scalar_< unspecified_<A1> > >)
-                                      (scalar_< integer_<A2> >)
-                                    )
-  {
-    typedef void result_type;
 
-    BOOST_FORCEINLINE result_type
-    operator()(A0 a0, A1 a1, A2 a2) const
-    {
-      _mm_stream_si32(reinterpret_cast<int*>(a1+a2), static_cast<int>(a0));
-    }
-  };
-
-  /// INTERNAL ONLY - Integer stream without offset
-  BOOST_DISPATCH_IMPLEMENT          ( stream_, tag::cpu_
-                                    , (A0)(A1)
-                                    , (scalar_< ints32_<A0> >)
-                                      (iterator_< scalar_< unspecified_<A1> > >)
-                                    )
-  {
-    typedef void result_type;
-
-    BOOST_FORCEINLINE result_type
-    operator()(A0 a0, A1 a1) const
-    {
-      _mm_stream_si32(reinterpret_cast<int*>(a1), static_cast<int>(a0));
-    }
-  };
 } } }
 
 #endif
