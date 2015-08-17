ec30932c53748980091ef6171144e2fe118c9b7a
Kenny Peou <kenny.peou@numscale.com>
2015-08-17 17:45:00 +0200

    device -> device assignment now works

--- modules/arch/opencl/include/nt2/memory/functions/opencl/copy.hpp
+++ modules/arch/opencl/include/nt2/memory/functions/opencl/copy.hpp
@@ -46,9 +46,73 @@ void copy(const opencl_buffer<T> & from, opencl_buffer<T> & into)
   compute::copy(from._vec.begin(), from._vec.end(), into._vec.begin());
 }
 
+template<class T>
+void copy(opencl_buffer<T> & into , const opencl_buffer<T> & from)
+{
+  if ( !from.size() ) return;
+  if ( into.size() != from.size() )
+    into.resize(from.size());
+
+  compute::copy(from._vec.begin(), from._vec.end(), into._vec.begin());
+}
+
 }} // end namespaces
 
 #endif
 
 #endif
 
+//#ifndef NT2_MEMORY_FUNCTIONS_OPENCL_COPY_HPP_INCLUDED
+//#define NT2_MEMORY_FUNCTIONS_OPENCL_COPY_HPP_INCLUDED
+//
+//#include <boost/dispatch/details/auto_decltype.hpp>
+//
+//#if defined(NT2_HAS_OPENCL)
+//
+//#include <nt2/sdk/memory/opencl/buffer.hpp>
+//#include <nt2/include/functions/size.hpp>
+//#include <boost/compute/container/vector.hpp>
+//#include <nt2/core/settings/locality.hpp>
+//
+//namespace nt2 { namespace memory
+//{
+//namespace compute = boost::compute;
+//
+//template<class T> class opencl_buffer;
+//
+//template<typename Container, class T>
+//void copy(Container & into, const opencl_buffer<T> & from)
+//{
+//  if ( !from.size() ) return;
+//  if ( into.size() != from.size() )
+//    into.resize(from.size());
+//
+//  compute::copy(from._vec.begin(), from._vec.end(), into.begin());
+//}
+//
+//template<typename Container, class T>
+//void copy(opencl_buffer<T> & into, const Container & from)
+//{
+//  if ( !from.size() ) return;
+//  if ( into.size() != from.size() )
+//    into.resize(from.size());
+//
+//  compute::copy(from.begin(), from.end(), into._vec.begin());
+//}
+//
+//template<class T>
+//void copy(opencl_buffer<T> & into, const opencl_buffer<T> & from)
+//{
+//  if ( !from.size() ) return;
+//  if ( into.size() != from.size() )
+//    into.resize(from.size());
+//
+//  compute::copy(from._vec.begin(), from._vec.end(), into._vec.begin());
+//}
+//
+//}} // end namespaces
+//
+//#endif
+//
+//#endif
+//
