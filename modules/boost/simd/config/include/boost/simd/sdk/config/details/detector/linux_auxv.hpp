9706d9dc17f553ee981bd39ef0360584a1ff8051
Joel Falcou <joel.falcou@lri.fr>
2016-02-17 10:27:06 +0100

    Missing include in auxv helper

--- modules/boost/simd/config/include/boost/simd/sdk/config/details/detector/linux_auxv.hpp
+++ modules/boost/simd/config/include/boost/simd/sdk/config/details/detector/linux_auxv.hpp
@@ -9,6 +9,8 @@
 #ifndef BOOST_SIMD_SDK_CONFIG_DETAILS_DETECTOR_LINUX_AUXV_HPP_INCLUDED
 #define BOOST_SIMD_SDK_CONFIG_DETAILS_DETECTOR_LINUX_AUXV_HPP_INCLUDED
 
+#include <boost/simd/sdk/config/os.hpp>
+
 #ifdef BOOST_SIMD_OS_LINUX
 #include <linux/auxvec.h>
 #include <fcntl.h>
